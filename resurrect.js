37dbcc1c5831d7d6af67e4a2190b577bbd886196
firm <firm@ARMY.lan>
2023-12-26 14:12:52 -0500

    fix

--- resurrect.js
+++ resurrect.js
@@ -1,5 +1,5 @@
 
 import { parse } from './index.min.js'
 import * as fs from 'fs'
-var data = fs.readFileSync('./ResurrectionCalendar.csv', 'utf8')
+var data = fs.readFileSync('./dates.csv', 'utf8')
 console.log(parse(data))
